898bff644b5dfc2628a963666f8d62d4927d5d6e
iqbal <iqbal@DESKTOP-CKDMN9M.home>
2020-04-21 16:33:35 -0400

    Hi, i attaced my works for your check thanks

---
+++ MyGitProject/src/com/talentech/Test.java
@@ -0,0 +1,13 @@
+package com.talentech;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 400;
+		int z = x+y;
+		System.out.println("Add value of x and y:"+z);
+
+	}
+
+}
